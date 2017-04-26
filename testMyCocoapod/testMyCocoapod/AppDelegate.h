b3814bde817e29f7d88a6e1e8e7239d26b531e40
orange5 <zxj852.611@qq.com>
2017-04-26 21:01:54 +0800

    完成工程创建

---
+++ testMyCocoapod/testMyCocoapod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testMyCocoapod
+//
+//  Created by orange on 26/04/2017.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testMyCocoapod/testMyCocoapod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testMyCocoapod
+//
+//  Created by orange on 26/04/2017.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
